adb5a9dc448d2dfeddfc9ef18adcaece17afbbb0
Dartrein <Dartrein@mail.ru>
2024-07-10 01:59:26 +0500

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBkg3LIT7Sd54or0siuJnPkeQiDvXZCOpE",
+    authDomain: "flame-buro.firebaseapp.com",
+    projectId: "flame-buro",
+    storageBucket: "flame-buro.appspot.com",
+    messagingSenderId: "726447790392",
+    appId: "1:726447790392:web:595666284f3703441a09cd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
